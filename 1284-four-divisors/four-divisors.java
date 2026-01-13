5f7784f4c78321b4dc29982d8f48180668d2bcdb
KSHITIJ BULE <157817510+KshitijBule@users.noreply.github.com>
2026-01-13 22:14:51 +0530

    Time: 1894 ms (5.00%) | Memory: 46.3 MB (66.15%) - LeetSync

---
+++ 1284-four-divisors/four-divisors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int sumFourDivisors(int[] nums) {
+        
+        int totalSum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            int count = 0;
+            int sum = 0;
+
+            for (int j = 1; j <= n; j++) {
+                if (n % j == 0) {
+                    count++;
+                    sum += j;
+                }
+                if (count > 4) break; // optimization
+            }
+
+            if (count == 4) {
+                totalSum += sum;
+            }
+        }
+        return totalSum;
+    }
+}
