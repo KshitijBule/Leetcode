6a97616705315ad16725453a4edef8822fcd38c0
KSHITIJ BULE <157817510+KshitijBule@users.noreply.github.com>
2026-01-17 19:34:05 +0530

    Time: 36 ms (21.37%) | Memory: 48.5 MB (56.32%) - LeetSync

--- 575-distribute-candies/distribute-candies.java
+++ 575-distribute-candies/distribute-candies.java
@@ -1,22 +1,34 @@
+
+
 class Solution {
-    public int distributeCandies(int[] arr) {
-        int n = arr.length/2;
-        int count = 0;
-       HashSet<Integer> set = new HashSet<>();
-        for(int i=0; i<arr.length; i++){
-            if(!set.contains(arr[i])){
-                set.add(arr[i]);
-            }
+    public int distributeCandies(int[] candyType) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int candy : candyType) {
+            set.add(candy);
         }
 
-        for(int x : set){
-            if(count<n){
-                count++;
-            }
-        }
-        return count;
+        int n = candyType.length;
+        return Math.min(set.size(), n / 2);
     }
 }
+
+
+// int n = arr.length/2;
+//         int count = 0;
+//        HashSet<Integer> set = new HashSet<>();
+//         for(int i=0; i<arr.length; i++){
+//             if(!set.contains(arr[i])){
+//                 set.add(arr[i]);
+//             }
+//         }
+
+//         for(int x : set){
+//             if(count<n){
+//                 count++;
+//             }
+//         }
+//         return count;
 
 //  int count = 1;
 //         int i = 0;
