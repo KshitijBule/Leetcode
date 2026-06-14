938c18351c3a362de8ce14c8d2471e616734f246
KSHITIJ BULE <157817510+KshitijBule@users.noreply.github.com>
2026-06-14 16:02:04 +0530

    Time: 12 ms (82.82%) | Memory: 44.2 MB (33.44%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -9,70 +9,44 @@
  */
 class Solution {
 
+    Map<TreeNode, TreeNode> parent = new HashMap<>();
+    List<Integer> ans = new ArrayList<>();
+
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
 
-        Map<TreeNode, TreeNode> parent = new HashMap<>();
-        buildParent(root, null, parent);
+        buildParent(root, null);
 
-        Queue<TreeNode> q = new LinkedList<>();
         Set<TreeNode> visited = new HashSet<>();
 
-        q.offer(target);
-        visited.add(target);
+        dfs(target, 0, k, visited);
 
-        int dist = 0;
-
-        while (!q.isEmpty()) {
-
-            int size = q.size();
-
-            if (dist == k) {
-                List<Integer> ans = new ArrayList<>();
-
-                while (!q.isEmpty()) {
-                    ans.add(q.poll().val);
-                }
-
-                return ans;
-            }
-
-            for (int i = 0; i < size; i++) {
-
-                TreeNode curr = q.poll();
-
-                if (curr.left != null && !visited.contains(curr.left)) {
-                    visited.add(curr.left);
-                    q.offer(curr.left);
-                }
-
-                if (curr.right != null && !visited.contains(curr.right)) {
-                    visited.add(curr.right);
-                    q.offer(curr.right);
-                }
-
-                if (parent.get(curr) != null &&
-                    !visited.contains(parent.get(curr))) {
-
-                    visited.add(parent.get(curr));
-                    q.offer(parent.get(curr));
-                }
-            }
-
-            dist++;
-        }
-
-        return new ArrayList<>();
+        return ans;
     }
 
-    private void buildParent(TreeNode node,
-                             TreeNode par,
-                             Map<TreeNode, TreeNode> parent) {
-
+    private void buildParent(TreeNode node, TreeNode par) {
         if (node == null) return;
 
         parent.put(node, par);
 
-        buildParent(node.left, node, parent);
-        buildParent(node.right, node, parent);
+        buildParent(node.left, node);
+        buildParent(node.right, node);
+    }
+
+    private void dfs(TreeNode node, int dist, int k,
+                     Set<TreeNode> visited) {
+
+        if (node == null || visited.contains(node))
+            return;
+
+        visited.add(node);
+
+        if (dist == k) {
+            ans.add(node.val);
+            return;
+        }
+
+        dfs(node.left, dist + 1, k, visited);
+        dfs(node.right, dist + 1, k, visited);
+        dfs(parent.get(node), dist + 1, k, visited);
     }
 }
